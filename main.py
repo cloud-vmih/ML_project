a0f7b09ef0304ee278c3a40b9d412792c3032ee9
dinhtruc15 <dinhtructhai@gmail.com>
2026-01-01 19:42:31 +0700

    data processing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.preproccess import concat_data, data_cleaning
 import os
+import pandas as pd
 
 def main():
     # Gom dữ liệu phim từ các file CSV
@@ -7,6 +8,17 @@ def main():
     output_path = f"{os.getcwd()}/data/raw/all_movies_data.csv"
     concat_data.concat_movie_data(base_path=base_path, output_path=output_path)
     
-    # Làm sạch dữ liệu
+    df = pd.read_csv(output_path) 
+    # (Khuyến nghị) ép kiểu Year để tránh lỗi
+    df["Year"] = pd.to_numeric(df["Year"], errors="coerce")
+
+    # Lọc dữ liệu theo năm (1975–2025)
+    df = df[(df["Year"] >= 1975) & (df["Year"] <= 2025)]
+
+    # Lưu lại dữ liệu sau khi lọc
+    filtered_output_path = f"{os.getcwd()}/data/raw/all_movies_data_1975_2025.csv"
+    df.to_csv(filtered_output_path, index=False)
+
+    
 if __name__ == "__main__":
     main()
